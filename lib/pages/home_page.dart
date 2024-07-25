5bcb3f0a73a0edb2f7401961fdbe90d5c77fa5f8
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-07-25 20:51:52 +0700

    creat bottom nav

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 
-class MyHome extends StatelessWidget {
-  const MyHome({super.key});
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePage createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHome> {
+  int _selectedIndex = 0;
+
+  void _navigateBottonBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('welcome', style: TextStyle(fontSize: 50),),
+      body: const Center(
+        child: Text(
+          'welcome',
+          style: TextStyle(fontSize: 50),
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottonBar,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.menu),
+             label: 'Menu'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_3_sharp),
+            label: 'Profile'),
+          ]),
     );
   }
 }
